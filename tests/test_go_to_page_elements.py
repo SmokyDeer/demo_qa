0df4874d7613d08066c848dec2aa5072a0c8520b
Anna <mini-anya@mail.ru>
2025-06-02 19:44:32 +0300

    demoqa test 2

---
+++ tests/test_go_to_page_elements.py
@@ -0,0 +1,14 @@
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+import time
+
+def test_go_to_page_elements(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementsPage(browser)
+
+
+    demo_qa_page.visit()
+    assert demo_qa_page.equal_url()
+    time.sleep(3)
+    demo_qa_page.button_elenents.click()
+    assert elements_page.equal_url()
